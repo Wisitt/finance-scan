34b656be0e4a922eac410ce4a65dfee2c036a40d
Wisit <wisit.m@devinnowave.co.th>
2025-03-06 17:55:52 +0700

    fix:bug

--- src/app/components/UserSelector.tsx
+++ src/app/components/UserSelector.tsx
@@ -10,7 +10,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+// import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import toast from 'react-hot-toast';
 import { useEffect } from 'react';
 
@@ -33,14 +33,14 @@ export default function UserSelector() {
   };
   
   // ฟังก์ชันสร้างตัวอักษรย่อจากชื่อ
-  const getInitials = (name: string) => {
-    return name
-      .split(' ')
-      .map(part => part[0])
-      .join('')
-      .toUpperCase()
-      .substring(0, 2);
-  };
+  // const getInitials = (name: string) => {
+  //   return name
+  //     .split(' ')
+  //     .map(part => part[0])
+  //     .join('')
+  //     .toUpperCase()
+  //     .substring(0, 2);
+  // };
   
   return (
     <div>
